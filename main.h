074c19be07e3dd587f6f1dec3f04b016929af579
Francesco <lunardifrancesco010@gmail.com>
2026-02-03 16:26:29 +0100

    Add files via upload

---
+++ main.h
@@ -0,0 +1,12 @@
+/* ---- FILES ".h" ---- */
+
+#include "Date.h"
+#include "Reservation.h"
+#include "Table.h"
+
+/* ---- LIBRARYS ---- */
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <ctime>
